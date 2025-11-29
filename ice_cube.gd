c0be3947961a1b5463766b2d734af7d28b337388
sadson keenolee <pawell4488@gmail.com>
2025-11-29 18:42:33 +0100

    lol

--- ice_cube.gd
+++ ice_cube.gd
@@ -6,20 +6,17 @@ var front_dir = Vector3.ZERO
 var force = Vector3.ZERO
 var aforce = Vector3.ZERO
 
-# --------- SKOK ŁADOWANY ---------
-var jump_charging := false
-var jump_charge := 0.0
-var jump_charge_max := 1.0           # ile sekund ładuje się skok
-var jump_power := 200000.0           # moc skoku (skalowana charge ratio)
+var jump_charge
 
 func _ready() -> void:
 	continuous_cd = true
+	gravity_scale = 5.0
 
 
 func _process(delta: float) -> void:
-	var jumped = false
 	force = Vector3.ZERO
 	aforce = Vector3.ZERO
+	var dir = Vector3.ZERO
 
 	# kierunek przodu względem kamery
 	front_dir = -$Camera3D.global_transform.basis.z
@@ -28,6 +25,7 @@ func _process(delta: float) -> void:
 
 	# --------- Która ściana jest dolna ---------
 	var floor_face = get_lowest_face()
+	print(floor_face)
 	match floor_face:
 		"up":
 			bottom_raycast = $RayCast_Up
@@ -41,76 +39,75 @@ func _process(delta: float) -> void:
 			bottom_raycast = $RayCast_Right
 		"left":
 			bottom_raycast = $RayCast_Left
-
-	# NIE RUSZAĆ
+		
 	if bottom_raycast == null or not bottom_raycast.is_colliding():
-		pass
-
-
-	# --------- RUCH WASD ---------
-	if Input.is_action_pressed("go_forward"):
-		force += front_dir
-	if Input.is_action_pressed("go_back"):
-		force -= front_dir
-	if Input.is_action_pressed("go_right"):
-		force += front_dir.rotated(Vector3.UP, -PI/2)
-	if Input.is_action_pressed("go_left"):
-		force += front_dir.rotated(Vector3.UP, PI/2)
-
-	# --------- ŁADOWANIE SKOKU ---------
-	if Input.is_action_pressed("jump") and bottom_raycast != null and bottom_raycast.is_colliding():
-		jump_charging = true
+		print(bottom_raycast)
+		print(not bottom_raycast.is_colliding())
+		return
+	
+	
+	if Input.is_action_just_pressed("jump"):
+		jump_charge = 0
+		return
+	if Input.is_action_pressed("jump"):
 		jump_charge += delta
-		print(jump_charge)
-		jump_charge = min(jump_charge, jump_charge_max)
+		return
+		
+	if Input.is_action_pressed("go_forward"):
+		dir += front_dir
+	if Input.is_action_pressed("go_back"):
+		dir -= front_dir
+	if Input.is_action_pressed("go_right"):
+		dir += front_dir.rotated(Vector3.UP, -PI/2)
+	if Input.is_action_pressed("go_left"):
+		dir += front_dir.rotated(Vector3.UP, PI/2)
+	
+	dir = dir.normalized()
+	
+	if Input.is_action_just_released("jump"):
+		force = jump(min(jump_charge,1), dir)*200
+		aforce = dir_to_aforce(dir)
+		jump_charge = 0
+		return
 
-	# ZWOLNIENIE SPACJI → SKOK
-	if Input.is_action_just_released("jump") and jump_charging:
-		jumped = true
-		jump_charging = false
-
-	if jumped:
-		var charge_ratio = jump_charge / jump_charge_max
-		var jump_strength = jump_power * charge_ratio
-
-		# pionowa siła skoku
-		force += Vector3.UP * jump_strength
-
-		# rotacja kostki – wokół osi prostopadłej do kierunku ruchu
-		if force.length() > 0:
-			var axis = force.cross(Vector3.UP).normalized()
-			aforce = axis * (jump_strength * 0.001)
-
-		jump_charge = 0.0
-
+	force = dir*10
 
 func _physics_process(delta: float) -> void:
-	# unikamy normalizowania zerowego wektora
 	if force.length() > 0:
-		apply_central_force(force.normalized() * 5000)
+		apply_central_force(force*10000)
 
-	apply_torque_impulse(aforce)
+	apply_torque_impulse(aforce*10000)
 
 
 func get_lowest_face() -> String:
-	var local_down = -transform.basis.y
+	var down = -Vector3.UP  # światowy dół
 
 	var directions = {
-		"up": Vector3.UP,
-		"down": Vector3.DOWN,
-		"left": Vector3.LEFT,
-		"right": Vector3.RIGHT,
-		"front": Vector3.FORWARD,
-		"back": Vector3.BACK
+		"up": transform.basis.y,
+		"down": -transform.basis.y,
+		"left": -transform.basis.x,
+		"right": transform.basis.x,
+		"front": -transform.basis.z,
+		"back": transform.basis.z
 	}
 
 	var lowest_face = "none"
 	var max_dot = -2.0
 
 	for face in directions.keys():
-		var dot_val = local_down.dot(directions[face])
+		var dot_val = directions[face].dot(down)
 		if dot_val > max_dot:
 			max_dot = dot_val
 			lowest_face = face
 
 	return lowest_face
+	
+func jump(charge, dir):
+	force = Vector3.UP * charge * 5 + dir
+	return force
+
+func dir_to_aforce(dir):
+	if dir.length() == 0:
+		return Vector3.ZERO
+	else:
+		return -dir.cross(Vector3.UP).normalized()
